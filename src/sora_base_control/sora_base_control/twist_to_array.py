a6863a811b3dbf231a795cd5b60344e9d3a131a8
RobinWyffels <robin.wyffels@student.hogent.be>
2026-04-19 20:45:45 +0200

    sim settings adding bridge 2

--- src/sora_base_control/sora_base_control/twist_to_array.py
+++ src/sora_base_control/sora_base_control/twist_to_array.py
@@ -2,20 +2,22 @@
 import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Twist
-from std_msgs.msg import Float64MultiArray
+from geometry_msgs.msg import Point
 
 class TwistBridge(Node):
     def __init__(self):
         super().__init__('twist_bridge')
         self.sub = self.create_subscription(Twist, '/cmd_vel', self.cb, 10)
-        self.pub = self.create_publisher(Float64MultiArray, '/cmd_vel_array', 10)
-        self.get_logger().info("Twist to Float64MultiArray bridge started for Isaac Sim!")
+        # Publish a Point instead of an Array!
+        self.pub = self.create_publisher(Point, '/cmd_vel_point', 10)
+        self.get_logger().info("Twist to Point bridge started for Isaac Sim!")
 
     def cb(self, msg):
-        arr = Float64MultiArray()
-        # Pack only [Linear X, Linear Y, Angular Z]
-        arr.data = [msg.linear.x, msg.linear.y, msg.angular.z]
-        self.pub.publish(arr)
+        pt = Point()
+        pt.x = msg.linear.x   # Forward
+        pt.y = msg.linear.y   # Strafe
+        pt.z = msg.angular.z  # Spin
+        self.pub.publish(pt)
 
 def main(args=None):
     rclpy.init(args=args)
